11b831bdc9e341bda0e54881a6309697a93f6180
DWSNOOB <gitdivta@gmail.com>
2023-04-09 15:37:33 +0800

    Styling Templete

--- src/App.js
+++ src/App.js
@@ -9,49 +9,62 @@ class App extends React.Component{
   render(){
     return (
       <>
-        <div className='container'>
+        <div className='container py-5'>
           <div className='row'>
-              <div className='col-12'>
+              <div className='col-12 text-center'>
                  <h1>Feeduiten App</h1>
-                 <hr></hr>
-                 <h4>Rp.1.500.00,-</h4>
-                 <span className='title'>Sisa uang yang tersisa 75%</span>
+                 <hr className='w-75 mx-auto'></hr>
+                 <h2>Rp.1.500.00,-</h2>
+                 <span className='title-md'>Sisa uang yang tersisa 75%</span>
               </div>
           </div>
-          <div className='row'>
-              <div className='col-6'>
-                 <div className='icon-wrapper'>
-                   <i class="bi bi-wallet2"></i>
-                 </div>
-                 <span className='title'>Pemasukan</span>
-                 <h3>Rp. 2.000.000,-</h3>
-                 <span className='title text-ungu'>50</span><span className='title'>Transaksi</span>
+          <div className='row mt-3'>           
+            <div className='col-6'>
+              <div className='card-wraper p-4'>
+                  <div className='icon-wraper mb-1'>
+                    <i class="bi bi-wallet2"></i>
+                  </div>
+                  <span className='title-sm '>Pemasukan</span>
+                  <h3>Rp. 2.000.000,-</h3>
+                  <span className='title-sm text-ungu'>50 </span><span className='title-sm'>Transaksi</span>
               </div>
+            </div>  
+            <div className='col-6'>
+              <div className='card-wraper p-4'>
+                  <div className='icon-wraper mb-1'>
+                    <i class="bi bi-cash"></i>
+                  </div>
+                  <span className='title-sm '>Pengeluaran</span>
+                  <h3>Rp. 2.000.000,-</h3>
+                  <span className='title-sm text-ungu'>50 </span><span className='title-sm'>Transaksi</span>
+              </div>
+              
+            </div>  
+            
           </div>
-          <div className='row'>
+          
+          <div className='row mt-3'>
             <div className='col-12 d-flex justify-content-between align-items-center'>
-              <h2>Ringkasan Transaksi</h2>
+              <h4>Ringkasan Transaksi</h4>
               <div className='wrapper-button d-flex'>
-                <button>Pemasukan  <i class="bi bi-plus-square-fill"></i></button>
-                <button>Pengeluaran <i class="bi bi-dash-circle-fill"></i></button>
+                <button className='button btn-ungu px-3 py-2 me-2'>Pemasukan  <i class="bi bi-plus-square-fill"></i></button>
+                <button className='button btn-pink px-3 py-2'>Pengeluaran <i class="bi bi-dash-circle-fill"></i></button>
               </div>
             </div>
 
           </div>
-          <div className='row'>
+          <div className='row mt-4'>
             <div className='col-12 d-flex justify-content-between align-items-center'>
               <div className='d-flex align-items-center'>
-                <div className='icon-wrapper'>
+                <div className='icon-wraper'>
                 <i class="bi bi-wallet2"></i>
                 </div>
-                <div className='transaksi'>
+                <div className='transaksi ms-3 d-flex flex-column'>
                   <h6>Menerima Gaji</h6>
-                  <span className='title'>1 Juli</span>
+                  <span className='title-sm'>1 Juli</span>
                 </div>
-
-                
               </div>
-              <h5 className='money'>Rp1.000.000,-</h5>
+              <h5 className='money-in'>Rp1.000.000,-</h5>
             </div>
           </div>
         </div>
